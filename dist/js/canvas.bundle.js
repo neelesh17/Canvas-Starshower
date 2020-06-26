808c673a65f25b77decb142e73d4be55e568f2ae
Neelesh Thakur <nt00605@gmail.com>
2020-06-26 13:57:06 +0530

    finished star shower animation

--- dist/js/canvas.bundle.js
+++ dist/js/canvas.bundle.js
@@ -97,6 +97,22 @@
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _utils__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./utils */ "./src/js/utils.js");
 /* harmony import */ var _utils__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_utils__WEBPACK_IMPORTED_MODULE_0__);
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () {})); return true; } catch (e) { return false; } }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
@@ -124,52 +140,197 @@ addEventListener('resize', function () {
   init();
 }); // Objects
 
-var _Object = /*#__PURE__*/function () {
-  function Object(x, y, radius, color) {
-    _classCallCheck(this, Object);
+var Star = /*#__PURE__*/function () {
+  function Star(x, y, radius, color) {
+    _classCallCheck(this, Star);
 
     this.x = x;
     this.y = y;
     this.radius = radius;
     this.color = color;
+    this.velocity = {
+      x: (Math.random() - 0.5) * 10,
+      y: 3
+    };
+    this.gravity = 1;
+    this.friction = 0.8;
   }
 
-  _createClass(Object, [{
+  _createClass(Star, [{
     key: "draw",
     value: function draw() {
+      c.save();
       c.beginPath();
       c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
       c.fillStyle = this.color;
+      c.shadowColor = '#e3eaef';
+      c.shadowBlur = 20;
       c.fill();
       c.closePath();
+      c.restore();
+    }
+  }, {
+    key: "shatter",
+    value: function shatter() {
+      this.radius -= 3;
+
+      for (var i = 0; i < 8; i++) {
+        ministars.push(new Ministar(this.x, this.y, 2));
+      }
     }
   }, {
     key: "update",
     value: function update() {
+      if (this.y + this.radius + this.velocity.y > canvas.height - groundHeight) {
+        this.velocity.y = -this.velocity.y * this.friction;
+        this.shatter();
+      } else {
+        this.velocity.y += this.gravity;
+      }
+
+      if (this.x + this.radius + this.velocity.x > canvas.width || this.x - this.radius <= 0) {
+        this.velocity.x = -this.velocity.x * this.friction;
+        this.shatter();
+      }
+
+      this.x += this.velocity.x;
+      this.y += this.velocity.y;
       this.draw();
     }
   }]);
 
-  return Object;
-}(); // Implementation
+  return Star;
+}();
+
+var Ministar = /*#__PURE__*/function (_Star) {
+  _inherits(Ministar, _Star);
+
+  var _super = _createSuper(Ministar);
+
+  function Ministar(x, y, radius, color) {
+    var _this;
+
+    _classCallCheck(this, Ministar);
+
+    _this = _super.call(this, x, y, radius, color);
+    _this.velocity = {
+      x: _utils__WEBPACK_IMPORTED_MODULE_0___default.a.randomIntFromRange(-5, 5),
+      y: _utils__WEBPACK_IMPORTED_MODULE_0___default.a.randomIntFromRange(-15, 15)
+    };
+    _this.gravity = 0.1;
+    _this.friction = 0.8;
+    _this.ttl = 100; //time to live
+
+    _this.opacity = 1;
+    return _this;
+  }
+
+  _createClass(Ministar, [{
+    key: "draw",
+    value: function draw() {
+      c.save();
+      c.beginPath();
+      c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+      c.fillStyle = "rgba(227,234,239, ".concat(this.opacity, ")");
+      c.shadowColor = '#e3eaef';
+      c.shadowBlur = 20;
+      c.fill();
+      c.closePath();
+      c.restore();
+    }
+  }, {
+    key: "update",
+    value: function update() {
+      if (this.y + this.radius + this.velocity.y > canvas.height - groundHeight) {
+        this.velocity.y = -this.velocity.y * this.friction;
+      } else {
+        this.velocity.y += this.gravity;
+      }
+
+      this.x += this.velocity.x;
+      this.y += this.velocity.y;
+      this.ttl -= 1;
+      this.opacity -= 1 / this.ttl;
+      this.draw();
+    }
+  }]);
+
+  return Ministar;
+}(Star);
+
+function createMountainRange(mountainAmount, height, color) {
+  for (var i = 0; i < mountainAmount; i++) {
+    c.beginPath();
+    var mountainWidth = canvas.width / mountainAmount;
+    c.moveTo(i * mountainWidth, canvas.height);
+    c.lineTo(i * mountainWidth + mountainWidth + 325, canvas.height);
+    c.lineTo(i * mountainWidth + mountainWidth / 2, canvas.height - height);
+    c.lineTo(i * mountainWidth - 325, canvas.height);
+    c.fillStyle = color;
+    c.fill();
+    c.closePath();
+  }
+} // Implementation
 
 
-var objects;
+var backgroundGradiant = c.createLinearGradient(0, 0, 0, canvas.height);
+backgroundGradiant.addColorStop(0, 'rgba(23, 30, 38, 0.7)');
+backgroundGradiant.addColorStop(1, 'rgba(63, 88, 107, 0.7)');
+var stars;
+var ministars;
+var backgroundStars;
+var ticker = 0;
+var randomSpawnRate = 75;
+var groundHeight = 100;
 
 function init() {
-  objects = [];
+  stars = [];
+  ministars = [];
+  backgroundStars = [];
 
-  for (var i = 0; i < 400; i++) {// objects.push()
+  for (var i = 0; i < 150; i++) {
+    var x = Math.random() * canvas.width;
+    var y = Math.random() * canvas.height;
+    var radius = Math.random() * 3;
+    backgroundStars.push(new Star(x, y, radius, 'white'));
   }
 } // Animation Loop
 
 
 function animate() {
   requestAnimationFrame(animate);
-  c.clearRect(0, 0, canvas.width, canvas.height);
-  c.fillText('HTML CANVAS BOILERPLATE', mouse.x, mouse.y); // objects.forEach(object => {
-  //  object.update()
-  // })
+  c.fillStyle = backgroundGradiant;
+  c.fillRect(0, 0, canvas.width, canvas.height);
+  backgroundStars.forEach(function (backgroundStar) {
+    backgroundStar.draw();
+  });
+  createMountainRange(1, canvas.height - 50, '#384551');
+  createMountainRange(2, canvas.height - 200, '#283843');
+  createMountainRange(3, canvas.height - 400, '#26333E');
+  c.fillStyle = '#182028';
+  c.fillRect(0, canvas.height - groundHeight, canvas.width, groundHeight);
+  stars.forEach(function (star, index) {
+    star.update();
+
+    if (star.radius <= 0) {
+      stars.splice(index, 1);
+    }
+  });
+  ministars.forEach(function (ministar, index) {
+    ministar.update();
+
+    if (ministar.ttl == 0) {
+      ministars.splice(index, 1);
+    }
+  });
+  ticker++;
+
+  if (ticker % randomSpawnRate == 0) {
+    var radius = 15;
+    var x = Math.max(radius, Math.random() * canvas.width - radius);
+    stars.push(new Star(x, -100, radius, 'white'));
+    randomSpawnRate = _utils__WEBPACK_IMPORTED_MODULE_0___default.a.randomIntFromRange(75, 200);
+  }
 }
 
 init();
--- src/js/canvas.js
+++ src/js/canvas.js
@@ -1,73 +1,205 @@
-import utils from './utils'
+import utils from './utils';
 
-const canvas = document.querySelector('canvas')
-const c = canvas.getContext('2d')
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
 
-canvas.width = innerWidth
-canvas.height = innerHeight
+canvas.width = innerWidth;
+canvas.height = innerHeight;
 
 const mouse = {
   x: innerWidth / 2,
   y: innerHeight / 2
 }
 
-const colors = ['#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66']
+const colors = ['#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66'];
+
 
 // Event Listeners
 addEventListener('mousemove', (event) => {
-  mouse.x = event.clientX
-  mouse.y = event.clientY
+  mouse.x = event.clientX;
+  mouse.y = event.clientY;
 })
 
 addEventListener('resize', () => {
-  canvas.width = innerWidth
-  canvas.height = innerHeight
+  canvas.width = innerWidth;
+  canvas.height = innerHeight;
 
-  init()
+  init();
 })
 
 // Objects
-class Object {
+class Star {
   constructor(x, y, radius, color) {
-    this.x = x
-    this.y = y
-    this.radius = radius
-    this.color = color
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.velocity = {
+      x: (Math.random() -0.5) * 10, 
+      y:3
+    };
+    this.gravity = 1;
+    this.friction = 0.8;
   }
 
   draw() {
-    c.beginPath()
-    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-    c.fillStyle = this.color
-    c.fill()
-    c.closePath()
+    c.save();
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.fillStyle = this.color;
+    c.shadowColor = '#e3eaef';
+    c.shadowBlur = 20;
+    c.fill();
+    c.closePath();
+    c.restore();
+  }
+
+  shatter(){
+    this.radius -= 3;
+    for(let i=0; i<8; i++){
+      ministars.push(new Ministar(this.x, this.y, 2));
+    }
   }
 
   update() {
-    this.draw()
+    if(this.y + this.radius + this.velocity.y > canvas.height - groundHeight){
+      this.velocity.y = -this.velocity.y * this.friction;
+      this.shatter();
+    } else {
+      this.velocity.y += this.gravity;
+    }
+    if(this.x + this.radius + this.velocity.x > canvas.width || this.x - this.radius <= 0){
+      this.velocity.x = -this.velocity.x* this.friction;
+      this.shatter();
+    }
+
+    this.x += this.velocity.x;
+    this.y += this.velocity.y;
+
+    this.draw();
+  }
+}
+
+class Ministar extends Star {
+  constructor(x,y,radius,color){
+    super(x,y,radius,color);
+    this.velocity = {
+      x: utils.randomIntFromRange(-5,5), 
+      y: utils.randomIntFromRange(-15,15)
+    };
+    this.gravity = 0.1;
+    this.friction = 0.8;
+    this.ttl = 100 //time to live
+    this.opacity = 1;
+  }
+
+  draw() {
+    c.save();
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.fillStyle = `rgba(227,234,239, ${this.opacity})`;
+    c.shadowColor = '#e3eaef';
+    c.shadowBlur = 20;
+    c.fill();
+    c.closePath();
+    c.restore();
+  }
+
+  update() {
+    if(this.y + this.radius + this.velocity.y > canvas.height - groundHeight){
+      this.velocity.y = -this.velocity.y * this.friction;
+    } else {
+      this.velocity.y += this.gravity;
+    }
+
+    this.x += this.velocity.x;
+    this.y += this.velocity.y;
+
+    this.ttl -= 1;
+    this.opacity -= 1/this.ttl;
+
+    this.draw();
+  }
+}
+function createMountainRange(mountainAmount, height, color) {
+  for (let i = 0; i < mountainAmount; i++) {
+    c.beginPath();
+    const mountainWidth = canvas.width/mountainAmount;
+    c.moveTo(i * mountainWidth, canvas.height);
+    c.lineTo(i * mountainWidth + mountainWidth + 325, canvas.height);
+    c.lineTo(i * mountainWidth + mountainWidth/2, canvas.height - height);
+    c.lineTo(i * mountainWidth - 325, canvas.height);
+    c.fillStyle = color;
+    c.fill();
+    c.closePath();
   }
 }
 
 // Implementation
-let objects
-function init() {
-  objects = []
+const backgroundGradiant = c.createLinearGradient(0, 0, 0, canvas.height);
+backgroundGradiant.addColorStop(0, 'rgba(23, 30, 38, 0.7)');
+backgroundGradiant.addColorStop(1, 'rgba(63, 88, 107, 0.7)');
 
-  for (let i = 0; i < 400; i++) {
-    // objects.push()
+let stars;
+let ministars;
+let backgroundStars;
+let ticker = 0;
+let randomSpawnRate = 75;
+let groundHeight  = 100;
+
+function init() {
+  stars = [];
+  ministars = [];
+  backgroundStars = [];
+
+  for (let i = 0; i < 150; i++) {
+    const x = Math.random() * canvas.width;
+    const y = Math.random() * canvas.height;
+    const radius = Math.random() * 3;
+    backgroundStars.push(new Star(x, y, radius, 'white'));
   }
-}
+}  
 
 // Animation Loop
 function animate() {
-  requestAnimationFrame(animate)
-  c.clearRect(0, 0, canvas.width, canvas.height)
+  requestAnimationFrame(animate);
+  c.fillStyle = backgroundGradiant;
+  c.fillRect(0, 0, canvas.width, canvas.height);
 
-  c.fillText('HTML CANVAS BOILERPLATE', mouse.x, mouse.y)
-  // objects.forEach(object => {
-  //  object.update()
-  // })
+  backgroundStars.forEach(backgroundStar => {
+    backgroundStar.draw();
+  });
+  
+  createMountainRange(1,canvas.height - 50,'#384551');
+  createMountainRange(2,canvas.height - 200,'#283843');
+  createMountainRange(3,canvas.height - 400,'#26333E');
+
+  c.fillStyle = '#182028';
+  c.fillRect(0,canvas.height - groundHeight, canvas.width, groundHeight);
+
+  stars.forEach( (star, index) => {
+    star.update();
+    if(star.radius <= 0){
+      stars.splice(index, 1);
+    }
+  });
+
+  ministars.forEach((ministar, index) => {
+    ministar.update();
+    if(ministar.ttl == 0){
+      ministars.splice(index, 1);
+    }
+  });
+
+  ticker++;
+
+  if(ticker % randomSpawnRate == 0){
+    const radius = 15;
+    const x = Math.max(radius, Math.random() * canvas.width - radius);
+    stars.push(new Star(x, -100, radius , 'white'));
+    randomSpawnRate = utils.randomIntFromRange(75, 200);
+  }
 }
 
-init()
-animate()
+init();
+animate();
